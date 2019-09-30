37e9b6427bb329e8f154e0500743594e06457bec
Mathew Fournier <mat@matfournier.com>
2019-09-29 21:19:19 -0700

    updated ex5 with foldMap

--- src/main/scala/mf/exercises/Exercise5.scala
+++ src/main/scala/mf/exercises/Exercise5.scala
@@ -9,10 +9,10 @@ import mf.http.Http
 import scala.util.control.NonFatal
 
 /**
-  * Should not stop for any Http.getBatch failures
-  * Should fail if any BatchResponse.response element fails to parse
-  * Should collect all parsing failures and all network failures
-  */
+ * Should not stop for any Http.getBatch failures
+ * Should fail if any BatchResponse.response element fails to parse
+ * Should collect all parsing failures and all network failures
+ */
 class Exercise5(implicit ec: ExecutionContext) {
   def requestBatch(requests: List[Request], batchSize: Int): Future[(List[ServiceError], List[ParsedResponse])] = {
     val batches = requests.grouped(batchSize).toList.map(BatchRequest.apply)
@@ -21,15 +21,17 @@ class Exercise5(implicit ec: ExecutionContext) {
 
   def getAndParseBatch(batch: BatchRequest): Future[(List[ServiceError], List[ParsedResponse])] =
     Http.getBatch(batch).map(parseResponse(_, ParsedResponse.parser)).recoverWith {
-      case NonFatal(e) => Future.successful( (ServiceError(e.getMessage)::Nil, List.empty[ParsedResponse]))
+      case NonFatal(e) => Future.successful((ServiceError(e.getMessage) :: Nil, List.empty[ParsedResponse]))
     }
 
   private def parseResponse(responses: BatchResponse,
-                            parser: String => Try[ParsedResponse]): (List[ServiceError], List[ParsedResponse]) = {
-    responses.responses.map(raw => parser(raw.value).toEither match {
-      case Left(e) => (ServiceError(e.getMessage) :: Nil, Nil)
-      case Right(pr) => (Nil, pr :: Nil)
-    }).combineAll
-  }
+                            parser: String => Try[ParsedResponse]): (List[ServiceError], List[ParsedResponse]) =
+    responses.responses
+      .foldMap(
+        raw =>
+          parser(raw.value).fold(
+            e => (ServiceError(e.getMessage) :: Nil, Nil),
+            pr => (Nil, pr :: Nil)
+          )
+      )
 }
-
